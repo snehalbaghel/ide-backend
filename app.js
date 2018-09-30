2bfe4280ab56affed8576dbee52c68ad8015bdb4
Arnav Gupta <arnav@codingblocks.com>
2018-10-01 03:14:14 +0530

    fix csrf protection
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const logger = require ('morgan');
 const cookieParser = require ('cookie-parser');
 const bodyParser = require ('body-parser');
 const passport = require ('passport');
+const csurf = require('csurf')
 const passportConf = require ('./passport');
 const index = require ('./routes/index');
 const users = require ('./routes/users');
@@ -26,6 +27,7 @@ app.use (logger ('dev'));
 app.use (bodyParser.json ());
 app.use (bodyParser.urlencoded ({ extended: false }));
 app.use (cookieParser ());
+app.use(csurf({cookie: true}))
 passport.initialize()
 app.use (express.static (path.join (__dirname, 'public')));
 app.use (express.static (path.join (__dirname, '.well-known')));
