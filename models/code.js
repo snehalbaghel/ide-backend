136f138d1e1233497bc050fc196025bd7356bc11
Abhishek Gupta <hi@itsabhishek.me>
2018-10-09 23:01:28 +0530

    add title to code table

--- models/code.js
+++ models/code.js
@@ -1,7 +1,15 @@
 'use strict';
+const v4 = require('uuid/v4')
+
 module.exports = (sequelize, DataTypes) => {
   var code = sequelize.define('code', {
     language: DataTypes.TEXT,
+    title: {
+      type: DataTypes.STRING,
+      defaultValue: function () {
+        return v4().split('-').pop()
+      }
+    },
     code: DataTypes.TEXT,
     custom_input: DataTypes.TEXT,
     file_name: DataTypes.STRING
--- routes/code.js
+++ routes/code.js
@@ -1,8 +1,29 @@
 const express = require ('express'),
   U = require('../util/util'),
   DB = require ('../models'),
+  passport = require('passport'),
   router = express.Router ()
 ;
+
+router.get('/', passport.authenticate('bearer', {session: false}), (req, res, next) => {
+  const offset = req.query.offset || 0
+  const limit = req.query.limit || 20
+  const filter = JSON.parse(req.query.filter)
+
+  DB.code.findAndCountAll({
+    where: {
+      ...filter,
+      userId: req.user.id,
+    },
+    offset,
+    limit
+  }).then(({rows, count}) => {
+    res.json({
+      count,
+      codes: rows
+    })
+  })
+})
 
 router.get ('/:id', (req, res, next) => {
   DB.code.findById(req.params.id, {
