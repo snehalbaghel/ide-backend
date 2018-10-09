fc55784fb57b10aabd697d2e41960d915c42133b
Abhishek Gupta <hi@itsabhishek.me>
2018-10-10 00:13:13 +0530

    try to update code whenever we can

--- routes/code.js
+++ routes/code.js
@@ -27,15 +27,33 @@ router.get('/', passport.authenticate('bearer', {session: false}), (req, res, ne
 
 router.get ('/:id', (req, res, next) => {
   DB.code.findById(req.params.id, {
-    attributes: ['language', 'code', ['custom_input', 'customInput'], ['file_name', 'fileName'] ]
+    attributes: ['id', 'title', 'language', 'code', ['custom_input', 'customInput'], ['file_name', 'fileName'] ]
   })
   .then (code => res.json (code))
   .catch (error => console.error (error))
 })
 
 
-router.post('/', U.authenticateOrPass, (req, res, next) => {
-  const {language, code, customInput, filename} = req.body
+router.post('/', U.authenticateOrPass, async (req, res, next) => {
+  const {id, language, code, customInput, filename} = req.body
+
+  if (id && req.user.id) {
+    // we have a codeId and user is authenticated -> we can update this code 
+    // instead of creating a new one
+
+    //check if this code belongs to current user
+    const dbCode = await DB.code.findById(id)
+    if (dbCode && dbCode.userId === req.user.id) {
+      dbCode.set("language", language)
+      dbCode.set("code", code)
+      dbCode.set("custom_input", customInput)
+      dbCode.set("file_name", filename)
+      await dbCode.save()
+      return res.json(dbCode)
+    }
+  }
+
+  // else just create a new one
   DB.code.create({
     language,
     code,
