e04f106072480fb0d71c94828dbd8859d296312f
Jatin Katyal <jatin.katyal13@gmail.com>
2019-06-18 21:27:31 +0530

    fixes

--- routes/run.js
+++ routes/run.js
@@ -4,16 +4,10 @@ const passport = require('passport')
 const { run } = require('../util/judge')
 const base64 = require('base-64')
 
-router.options('*', (req, res) => {
-  res.status(204).send('OK')
-})
-
 router.post('/run', passport.authenticate('bearer', {session: false}), async (req, res) => {
   try {
     const { source, input, lang } = req.body
-    console.log('running')
     const resp = await run(lang, source, input)
-    console.log(resp)
     res.json(resp)
   } catch (err) {
     console.log(err)
--- util/auth.js
+++ util/auth.js
@@ -15,46 +15,6 @@ const get_auth_token = (nonce, secret) => {
   return (token);
 };
 
-const verify_token = (nonce, received_token, secret) => {
-  if (!nonce || nonce.indexOf('|')===-1) {
-    throw new Error({
-      message: 'Nonce in Wrong Format',
-      status: 403
-    })
-  }
-
-  const timestamp = nonce.split('|')[0]
-
-  const expiration_seconds = 300;
-  const tolerance_seconds = 60;
-
-  const now = Math.floor(Date.now()/1000);
-
-  if(timestamp >= now + tolerance_seconds){
-    throw new Error({
-      message: "API auth_token timestamp is too far in the future",
-      status: 403
-    })
-  } else if (timestamp < now - expiration_seconds){
-    throw new Error({
-      message: "API auth_token has expired.",
-      status: 403
-    })
-  }
-
-  const hash = crypto.createHash('sha256');
-  const auth_token = hash.update(String(nonce) + String(secret)).digest('hex');
-
-  if (!received_token || received_token !== auth_token) {
-    throw new Error({
-      message: "auth_token is missing or not valid",
-      status: 403
-    })
-  }
-
-  return true
-}
-
 module.exports = {
   get_nonce,
   get_auth_token,
