e8547c321def48e3f1a2bc6c59d7fcc7e9fada54
Jatin Katyal <jatin.katyal13@gmail.com>
2019-06-19 17:48:50 +0530

    import fixes

--- routes/run.js
+++ routes/run.js
@@ -1,8 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const passport = require('passport')
 const { run } = require('../util/judge')
-const base64 = require('base-64')
 
 router.post('/run', async (req, res) => {
   try {
