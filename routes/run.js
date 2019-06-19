b346765646bb8ed008991f1bb6b0c2dc3397b876
Jatin Katyal <jatin.katyal13@gmail.com>
2019-06-19 14:59:12 +0530

    allow not logged in users to run code

--- routes/run.js
+++ routes/run.js
@@ -4,7 +4,7 @@ const passport = require('passport')
 const { run } = require('../util/judge')
 const base64 = require('base-64')
 
-router.post('/run', passport.authenticate('bearer', {session: false}), async (req, res) => {
+router.post('/run', async (req, res) => {
   try {
     const { source, input, lang } = req.body
     const resp = await run(lang, source, input)
