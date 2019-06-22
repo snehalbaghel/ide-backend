361743f0a49d1d17b88a8592861ae4729b660893
Abhishek Gupta <hi@itsabhishek.me>
2019-06-22 17:35:58 +0530

    add rate limiter to /run

--- routes/run.js
+++ routes/run.js
@@ -2,9 +2,19 @@ const express = require('express')
 const router = express.Router()
 const passport = require('passport')
 const { run } = require('../util/judge')
-const base64 = require('base-64')
+const U = require('../util/util')
+const rateLimit = require("express-rate-limit");
 
-router.post('/run', async (req, res) => {
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 50, // limit each IP to 100 requests per windowMs
+  keyGenerator: (req) => {
+    return (req.user && req.user.id) || req.headers['x-real-ip'] || req.connection.remoteAddress;
+  }
+})
+
+
+router.post('/run', U.authenticateOrPass, limiter, async (req, res) => {
   try {
     const { source, input, lang } = req.body
     const resp = await run(lang, source, input)
